a8771a943f367cb16edf9ee207b3a0db6a6472d7
sidgittesting <47843246+sidgittesting@users.noreply.github.com>
2019-02-21 11:57:23 +0530

    Update testClientScript.js

--- Test-Project-21Feb2019/Test Project/testClientScript.js
+++ Test-Project-21Feb2019/Test Project/testClientScript.js
@@ -3,38 +3,38 @@
 * @NScriptType ClientScript
 */
 define(['N/ui/message'],
-function(msg) {
-function showErrorMessage(msgText) {
-var myMsg = msg.create({
-title: "Cannot Save Record",
-message: msgText,
-type: msg.Type.ERROR
-});
-myMsg.show({
-duration: 5000
-});
-}
-function saveRec(context) {
-var rec = context.currentRecord;
-var currentDate = new Date()
-var oneWeekAgo = new Date(currentDate - 1000 * 60 * 60 * 24 * 7);
-// Validate transaction date is not older than current time by one week
-if (rec.getValue({
-fieldId: 'trandate'
-}) < oneWeekAgo) {
-showErrorMessage("Cannot save sales order with trandate one week old.");
-return false;
-}
-//validate total is greater than 0
-if (rec.getValue({
-fieldId: 'total'
-}) <= 0) {
-showErrorMessage("Cannot save sales order with negative total amount.");
-return false;
-}
-return true;
-}
-return {
-saveRecord: saveRec
-}
-});
+    function (msg) {
+        function showErrorMessage(msgText) {
+            var myMsg = msg.create({
+                title: "Cannot Save Record",
+                message: msgText,
+                type: msg.Type.ERROR
+            });
+            myMsg.show({
+                duration: 5000
+            });
+        }
+        function saveRec(context) {
+            var rec = context.currentRecord;
+            var currentDate = new Date()
+            var oneWeekAgo = new Date(currentDate - 1000 * 60 * 60 * 24 * 7);
+            // Validate transaction date is not older than current time by one week
+            if (rec.getValue({
+                fieldId: 'trandate'
+            }) < oneWeekAgo) {
+                showErrorMessage("Cannot save sales order with trandate one week old.");
+                return false;
+            }
+            //validate total is greater than 0
+            if (rec.getValue({
+                fieldId: 'total'
+            }) <= 0) {
+                showErrorMessage("Cannot save sales order with negative total amount.");
+                return false;
+            }
+            return true;
+        }
+        return {
+            saveRecord: saveRec
+        }
+    });
